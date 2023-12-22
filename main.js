b6a4910cd5da003d594009282be4d143e76f542f
ChristianBaciu <christian3110@outlook.it>
2023-12-23 00:28:15 +0100

    sto impazzendo

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-let nomeUtente = prompt ("ciaociao");
+let nKM = prompt ("Quanti Km?","km");
+let viaggiatore = prompt ("Per favore iserisci l'età", "Età");
+const biglietto = 0.21;
+
+
+console.log("totKm" , "viaggiatore" , "biglietto")
+
+
+
+
+
+// function changeColor(newColor) {
+//     const elem = document.getElementById("para");
+//     elem.style.color = newColor;
+// }
