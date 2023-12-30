53b7d328dcba28632dd3766b4bcfa91657323331
ChristianBaciu <christian3110@outlook.it>
2023-12-30 18:42:05 +0100

    tofixed

--- main.js
+++ main.js
@@ -3,17 +3,14 @@ let passeggero = prompt ("Età del passeggero");
 let biglietto = 0.21;
 let totale = nKM * biglietto;
 
-
 if(passeggero < 18){
-    totale = (totale * 0.2)
+    totale = (totale * 0.2).toFixed(2)
 }
 else if(passeggero > 65){
-    totale = (totale * 0.4)
+    totale = (totale * 0.4).toFixed(2)
 }
-
 
 document.writeln("il costo totate del biglietto è di" + " " + totale + "€")
 console.log("nKM")
 console.log("passeggero")
 console.log("0.21")
-
